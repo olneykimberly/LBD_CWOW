90c058c50248b09f2375e0936f94cc50649fa11d
olneykimberly <olneykimberly@gmail.com>
2022-02-28 11:00:15 -0600

    directories

--- scripts/snakemake/create_config.py
+++ scripts/snakemake/create_config.py
@@ -5,38 +5,27 @@ outfile = open('config.json', 'w')
 
 # get all, kidney, brain and blood sample names
 allSamples = list()
-brainSamples = list()
-kidneySamples = list()
-bloodSamples = list()
+type_LBD = list()
+LDB_type = list()
+sex = list()
 read = ["R1", "R2"]
 numSamples = 0
 
-with open('sampleReadGroupInfo.txt', 'r') as infile:
+with open('sample_read_group_info.txt', 'r') as infile:
     for line in infile:
         numSamples += 1
 
-        line = line.replace("-", "_")
+        line = line.replace(".", "_")
         split = line.split()
-        sampleAttributes = split[0].split('_')  # uniqueNum_pigID_tissue_XX_XX_sequencer_lane_read_001.fastq.gz
-
+        sampleAttributes = split[0].split('_') # NA00-190.FCH5MYFDMXY_L1_R1_ITCTCTACT-CGCGGTTC.fastq.gz 
         # create a shorter sample name
-        stemName = sampleAttributes[0] + '_' + sampleAttributes[1] + '_' + sampleAttributes[2] + '_' + \
-                   sampleAttributes[5] + '_' + sampleAttributes[6]
-
+        stemName = sampleAttributes[0] + '_' + sampleAttributes[1] + '_' + sampleAttributes[2]
         allSamples.append(stemName)
-        if stemName.__contains__("Brain"):
-            brainSamples.append(stemName)
-        elif line.__contains__("Kidney"):
-            kidneySamples.append(stemName)
-        elif line.__contains__("BB"):
-            bloodSamples.append(stemName)
-        elif line.__contains__("FB"):
-            bloodSamples.append(stemName)
 
 # create header and write to outfile
 header = '''{{
     "Commment_Input_Output_Directories": "This section specifies the input and output directories for scripts",
-    "rawReads" : "/research/labs/neurology/fryer/projects/sepsis/pig/LPS/bulkRNA/",
+    "rawReads" : "/research/labs/neurology/fryer/projects/LBD_CWOW/bulkRNA/",
     "rawQC" : "../../rawQC/",
     "trimmedReads" : "../../trimmedReads/",
     "trimmedQC" : "../../trimmedQC/",
@@ -45,31 +34,28 @@ header = '''{{
     "featureCounts" : "../../featureCounts/",
     "kallisto" : "../../kallisto/",
     "salmon" : "../../salmon/", 
-    "kraken" : "../../kraken/", 
-    "kraken_taxo" : "/usr/local/biotools/kraken2/v2.1.1/databases/taxo.k2d",
 
     "Comment_Reference" : "This section specifies the location of the Sus scrofa, Ensembl reference genome",
-    "Sscrofa.Ymasked.fa" : "/research/labs/neurology/fryer/projects/references/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.Ymasked",
-    "Scrofa.cdna.Ymasked.fa" : "/research/labs/neurology/fryer/projects/references/pig/Sus_scrofa.Sscrofa11.1.cdna.all.Ymasked",
-    "Sscrofa.fa" : "/research/labs/neurology/fryer/projects/references/pig/Sscrofa11.1.dna.toplevel",
-    "Sscrofa.gtf" : "/research/labs/neurology/fryer/projects/references/pig/Sus_scrofa.Sscrofa11.1.103",
-    "Sscrofa_names_protein.gtf" : "/research/labs/neurology/fryer/projects/references/pig/Sscrofa_geneNames_proteinCoding",
-    "salmon_ref_index" : "/research/labs/neurology/fryer/projects/references/pig/salmon_index_Ymasked",
-    "star_ref_index" : "/research/labs/neurology/fryer/projects/references/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.Ymasked",
-    "kallisto_ref_index" : "/research/labs/neurology/fryer/projects/references/pig/Sus_scrofa.Sscrofa11.1.cdna.all.Ymasked.kallisto",
+    "GRCh38.fa" : "/research/labs/neurology/fryer/projects/references/human/GRCh38.primary_assembly.genome",
+    "GRCh38.gtf" : "/research/labs/neurology/fryer/projects/references/human/gencode.v38.annotation",
+    
+    "GRCh38.Ymasked.fa" : "/research/labs/neurology/fryer/projects/references/human/GRCh38_Ymasked_XX.fa",
+    "GRCh38.YPARs_masked.fa" : "/research/labs/neurology/fryer/projects/references/human/GRCh38_YPARsmasked_XY.fa",
+
+    
+    "GRCh38.Ymasked.star" : "/research/labs/neurology/fryer/projects/references/human/GRCh38_YPARsmasked_XY_STAR",
+    "GRCh38.YPARs_masked.star" : "/research/labs/neurology/fryer/projects/references/human/GRCh38_YPARsmasked_XY_STAR",
+
 
     "Comment_Sample_Info": "The following section lists the samples that are to be analyzed",
     "sample_names": {0},
-    "brain_names": {1},
-    "kidney_names": {2},
-    "blood_names": {3},
-    "read": {4},
+    "read": {1},
 '''
-outfile.write(header.format(allSamples, brainSamples, kidneySamples, bloodSamples, read))
+outfile.write(header.format(allSamples, read))
 
 # config formatting
 counter = 0
-with open('sampleReadGroupInfo.txt', 'r') as infile:
+with open('sample_read_group_info.txt', 'r') as infile:
     for line in infile:
         counter += 1
         # store sample name and info from the fastq file
@@ -78,17 +64,17 @@ with open('sampleReadGroupInfo.txt', 'r') as infile:
         base = base.replace(".fastq.gz", "")
         sampleName1 = base
         sampleName2 = sampleName1.replace("R1","R2")
-        base = base.replace("_R1_001", "")
+        base = base.replace("_R1_", "")
         sampleInfo = split[1]
 
         # make naming consistent, we will rename using only underscores (no hyphens)
-        line = line.replace("-", "_")
+        line = line.replace(".", "_")
         split = line.split()
-        sampleAttributes = split[0].split('_')  # uniqueNum_pigID_tissue_XX_XX_sequencer_lane_read_001.fastq.gz
+        sampleAttributes = split[0].split('_')  # NA00-190.FCH5MYFDMXY_L1_R1_ITCTCTACT-CGCGGTTC.fastq.gz
+        # uniqueNum-number_sequencer_lane_read.fastq.gz
 
         # create a shorter sample name
-        stemName = sampleAttributes[0] + '_' + sampleAttributes[1] + '_' + sampleAttributes[2] + '_' + \
-                   sampleAttributes[5] + '_' + sampleAttributes[6]
+        stemName = sampleAttributes[0] + '_' + sampleAttributes[1] + '_' + sampleAttributes[2]
         shortName1 = stemName + '_R1'
         shortName2 = stemName + '_R2'
 
@@ -108,7 +94,7 @@ with open('sampleReadGroupInfo.txt', 'r') as infile:
 
         out = '''
     "{0}":{{
-        "fq_path": "/research/labs/neurology/fryer/projects/sepsis/pig/LPS/bulkRNA/",
+        "fq_path": "/research/labs/neurology/fryer/projects/LBD_CWOW/bulkRNA/",
         "fq1": "{1}",
         "fq2": "{2}",
         "shortName1": "{3}",
