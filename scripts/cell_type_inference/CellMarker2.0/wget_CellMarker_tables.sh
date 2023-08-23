c59644f0a666ef8de0cdf3a22000688c0623d798
olneykimberly <olneykimberly@gmail.com>
2023-08-23 11:55:44 -0500

    manuscript figs, and supplemental

---
+++ scripts/cell_type_inference/CellMarker2.0/wget_CellMarker_tables.sh
@@ -0,0 +1,12 @@
+# Cell Marker 2.0 data download page
+# http://bio-bigdata.hrbmu.edu.cn/CellMarker/CellMarker_download.html
+
+# All cell markers - All cell markers of different cell types from different tissues in human and mouse.
+wget http://bio-bigdata.hrbmu.edu.cn/CellMarker/CellMarker_download_files/file/Cell_marker_All.xlsx
+# get human cell markers - Cell markers of different cell types from different tissues in human.
+wget http://bio-bigdata.hrbmu.edu.cn/CellMarker/CellMarker_download_files/file/Cell_marker_Human.xlsx
+# mouse - Cell markers of different cell types from different tissues in mouse.
+wget http://bio-bigdata.hrbmu.edu.cn/CellMarker/CellMarker_download_files/file/Cell_marker_Mouse.xlsx
+# single cell - Cell markers derived from single-cell sequencing researches in human and mouse.
+wget http://bio-bigdata.hrbmu.edu.cn/CellMarker/CellMarker_download_files/file/Cell_marker_Seq.xlsx
+
---
+++ scripts/cell_type_inference/TheHumanProteinAtlas/wget_ProteinAtlas_tables.sh
@@ -0,0 +1,7 @@
+# The Human Protein Atlas Data Download page
+
+
+# RNA single cell type data -
+# Transcript expression levels summarized per gene in 79 cell types from 30 datasets. The tab-separated file includes Ensembl gene identifier ("Gene"), gene name ("Gene name"), analysed sample ("Cell type") and normalized expresion ("nTPM").
+wget https://www.proteinatlas.org/download/rna_single_cell_type.tsv.zip
+
